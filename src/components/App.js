ae1937e0d2ec188904c9aaaaacfbd93400177115
Vikash kumar <64901451+vikash1837@users.noreply.github.com>
2021-03-12 00:03:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,18 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	    this.state={
+		   paragraph:""
+	    }
 	};
-
+	Handlechange=()=>{
+		this.setState({paragraph: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"})
+	}
     render() {
     	return(
     		<div id="main">
+		<p id="para">{this.state.paragraph}</p>
+		<button id="click" onClick={this.HandleChange}></button>
 				{ /* Do not remove this main div!! */ }
     		</div>
     	);
